0e1983542babfcd60a30f210516023a1f861f03c
Peter Chim <pchim24@gmail.com>
2016-04-30 13:22:51 -0700

    Add bg, dynamic sky, and dancers

--- visualize.js
+++ visualize.js
@@ -1,7 +1,7 @@
 var visualize = function() {
 	var freqData = new Uint8Array(1024);
 	// set up and create svg
-	var svgHeight = 300;
+	var svgHeight = 600;
 	var svgWidth = 1024;
 	var circleRadius = 3;
 	var createSVG = function(parent, height, width){
@@ -11,10 +11,69 @@ var visualize = function() {
 						 .enter()
 						 .append('svg')
 						 .attr('height', height)
-						 .attr('width', width);
-
+						 .attr('width', width)
+						 .classed('sky', true);
 	}
+
+	// create the floor
 	var svg = createSVG('body', svgHeight, svgWidth);
+
+	// set up background sky
+	var bgLight = 1;
+	var bgHsl = "hsl(185, 100%," + "5" + '%)';
+	var bgSun = 'up';
+	var bgLow = svg.selectAll('rect')
+							.data([0])
+							.enter()
+							.append('rect')
+							.attr('width', '100%')
+							.attr('height', '100%')
+							.attr('fill', function(d, i) {
+		 	 					return bgHsl; });
+	var bgHsl = "hsl(185, 100%," + "70" + '%)';
+	var bg = svg.append('rect')
+							.attr('width', '100%')
+							.attr('height', '50%')
+							.attr('fill', function(d, i) {
+		 	 					return bgHsl; })
+							.attr('opacity', bgLight);
+
+	// create the floor
+	svg.selectAll('image')
+		 .data(['assets/beach.jpeg'])
+		 .enter()
+		 .append('image')
+		 .attr('height', svgHeight/2)
+		 .attr('width', svgWidth)
+		 .attr('x', '0')
+		 .attr('y', svgHeight/2)
+		 .attr('preserveAspectRatio', 'none')
+		 .attr('xlink:href', function(d) { return d; });
+
+	// transition background color
+	var bgChange = function(){
+		// 15 seconds
+		var transitionTime = 15000;
+		console.log(bgHsl);
+		if (bgSun === 'up'){
+			if (bgLight === 0) {
+				bgLight = 1;
+			} else if (bgLight === 1) {
+				bgLight = 0;
+			}
+ 			// bgHsl = "hsl(185, 100%," + bgLight + '%)';
+ 			bgSun = 'down';
+			bg.transition().duration(transitionTime - 1)
+				.attr('opacity', bgLight);
+				// .attr('fill', function(d, i) {
+		 	//  		return bgHsl; });
+			setTimeout(bgChange, transitionTime);
+		} else if (bgSun === 'down'){
+			bgSun = 'up';
+			setTimeout(bgChange, transitionTime / 2);
+		} 
+	};
+
 
 	// set up color nodes
 	svg.selectAll('circle')
@@ -25,7 +84,7 @@ var visualize = function() {
 		 	 return i;
 		 })
 		 .attr('cy', function(d, i) {
-		 	 return svgHeight - 5;
+		 	 return svgHeight/2 - 5;
 		 })
 		 .attr('r', circleRadius)
 		 .attr('fill', function(d, i) {
@@ -47,18 +106,69 @@ var visualize = function() {
 	var visColors = function(){
 		// run fn before repainting to screen
 		requestAnimationFrame(visColors);
-
 		// get frequency data
 		analyser.getByteFrequencyData(freqData);
 
 		svg.selectAll('circle.node')
 			 .data(freqData)
 			 .attr('cy', function(d) {
-			 	return svgHeight - d;
-			 })
+			 	return svgHeight/2 - d;
+			 });
 	};
+
+	// add dancers
+
+	var gifs = 'gifs/'
+	var constDancers = ['gifs/bear.gif',
+											'gifs/light.gif',
+											'gifs/link_dance.gif',
+											'gifs/panda.gif',
+											'gifs/pikachu.gif',
+											'gifs/shark.gif'];
+	var allDancers = [];
+	svg.selectAll('image.dancer')
+							.data(allDancers)
+							.enter()
+							.append('image')
+							.classed('dancer', true)
+							.attr('height', 100)
+							.attr('width', 100)
+							.attr('x', Math.random()*svgWidth)
+							.attr('y', svgHeight/2 + Math.random()*(svgHeight/2))
+							.attr('xlink:href', function(d) { return d; });
+
+	// add dancer oon click
+	var addDancers = function() {
+		var randomDancer = constDancers[Math.floor(Math.random()*constDancers.length)];
+		allDancers.push(randomDancer);
+		svg.selectAll('image.dancer')
+							.data(allDancers)
+							.enter()
+							.append('image')
+							.classed('dancer',true)
+							.attr('height', 80)
+							.attr('width', 80)
+							.attr('x', Math.random()*svgWidth)
+							.attr('y', svgHeight/2 + Math.random()*(svgHeight/2))
+							.attr('xlink:href', function(d) { return d; });		
+	};
+
+	var stepDancers = function() {
+
+	};
+
+	d3.select('.add-dancer')
+		.on('click', function() {
+			addDancers();
+	});
+
+	bgChange();
 	visColors();
 
+
+
+
 };
+
 
 visualize();
